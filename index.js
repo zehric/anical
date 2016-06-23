86c0fd429e074d362f02867a35ed41e07e5a8038
Eric Zhou <ericfzhou@berkeley.edu>
2016-06-23 03:16:46 -0700

    editing dates for experiment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var cal = function() {
     } else {
       var calendar = {};
       var anime = JSON.parse(data);
-      for (let i = -6; i < 7; i++) {
+      for (let i = -7; i < 6; i++) {
         calendar[moment().add(i, 'days').format('dddd, YYYY-MM-DD')] = [];
       }
       var now = moment();
@@ -18,7 +18,7 @@ var cal = function() {
         for (let episode in anime[name]) {
           var time = moment.unix(anime[name][episode]);
           var difference = time.diff(now, 'days');
-          if (difference > -6 && difference < 7) {
+          if (difference > -7 && difference < 6) {
             calendar[time.format('dddd, YYYY-MM-DD')].push(time.format('hh:mm')
               + ' | ' + name + ' <EP. ' + episode + '>');
           }
